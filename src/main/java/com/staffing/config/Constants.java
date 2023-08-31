a85f6c09ae728f6f73efe016da874f86544b34b0
soungalo <soungalo.yeo@groupehillstone.com>
2023-08-31 17:34:56 +0200

    Initial version of StaffingMsManagement generated by generator-jhipster@8.0.0-beta.2

---
+++ src/main/java/com/staffing/config/Constants.java
@@ -0,0 +1,11 @@
+package com.staffing.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
---
+++ src/main/java/com/staffing/web/rest/errors/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer error handling.
+ */
+package com.staffing.web.rest.errors;
